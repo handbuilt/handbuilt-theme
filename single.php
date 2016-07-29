1c43ccc6fc0aa3c785d9c93ef18612165ea9e095
Daniel Bachhuber <daniel@handbuilt.co>
2016-07-29 11:34:48 -0700

    Fix closing bracket

--- single.php
+++ single.php
@@ -9,7 +9,7 @@
 
 				<?php while( have_posts() ) : the_post(); ?>
 					
-					<article <?php post_class(); ?>
+					<article <?php post_class(); ?>>
 
 						<header class="page-header">
 							<h2><?php the_title(); ?><?php edit_post_link( ' <small><i class="fa fa-pencil"></i></small>' ); ?></h2>
