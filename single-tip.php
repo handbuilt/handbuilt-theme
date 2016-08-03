f954987b60166388ef2a1520a33d8aee7e03915a
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-03 06:07:45 -0700

    Add a single tip view

---
+++ single-tip.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+	<div class="site-content">
+
+		<?php if ( have_posts() ) : ?>
+
+			<div class="row">
+				<div class="columns medium-8 medium-centered">
+
+				<?php while( have_posts() ) : the_post(); ?>
+
+					<article <?php post_class(); ?>>
+
+						<header class="page-header">
+							<?php $post_type_object = get_post_type_object( get_post_type() ); ?>
+							<h5><a href="<?php echo esc_url( get_post_type_archive_link( get_post_type() ) ); ?>"><?php echo esc_html( $post_type_object->label ); ?></a> - <?php echo esc_html( $post_type_object->description ); ?></h5>
+							<h2><?php the_title(); ?><?php edit_post_link( ' <small><i class="fa fa-pencil"></i></small>' ); ?></h2>
+						</header>
+
+						<div class="page-content">
+
+							<?php the_content(); ?>
+						</div>
+
+					</article>
+
+				<?php endwhile; ?>
+
+				</div>
+
+			</div>
+
+		<?php endif; ?>
+
+		<?php if ( ! is_home() ) : ?>
+			<?php echo Hand_Built::get_template_part( 'branded-intro' ); ?>
+		<?php endif; ?>
+
+	</div>
+
+<?php get_footer(); ?>
